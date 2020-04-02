d3448ad377aaabb058fa4319f6417c574ac0a55e
zymk99 <243817437@qq.com>
2020-04-02 10:39:29 +0800

    普通跟进

--- src/main/resources/static/admin/my-html/pictureToText.js
+++ src/main/resources/static/admin/my-html/pictureToText.js
@@ -15,7 +15,19 @@ var picToText_up=new Vue({
         handleAvatarSuccess:function(res, file) {
             this.imageUrl = URL.createObjectURL(file.raw);
             var value=Zafkiel.request("/data/picToText",null,"post");
-            debugger
+            if(value)
+            {
+                debugger
+                var text="";
+                var text2="";
+                var data=JSON.parse(value).data.image_records;
+                for(var i=0;i<data.length;i++)
+                {
+                    text+=(data[i].source_text+"\n");
+                    text2 +=(data[i].target_text+"\n");
+                }
+                picToText_text._data.textarea1=text;
+            }
         }
     }
 })
