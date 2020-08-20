a4a2454bebfd1494e9a6992702a8247ca3da7e4e
ni <243817437@qq.com>
2020-08-20 22:28:45 +0800

    日常

--- src/main/resources/static/admin/dom/Waterfall/index.vue
+++ src/main/resources/static/admin/dom/Waterfall/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div >
-        <div v-for="item in rownum">{{item}}</div>
+        <div v-for="item in myrownum">{{item}}</div>
     </div>
 </template>
 
@@ -9,8 +9,39 @@
         props:{
             rownum:Number
         },
-        data:{
+        data(){             //自定义组件的data是个方法
+            return{
+                myrownum:this.rownum
+            }
+        },
+        created(){
 
+        },
+        mounted(){                    //组件装配后
+            this.AdditionalData();
+        },
+        methods:{
+            //是否触底
+            TouchBottom(){
+                let scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+                //变量windowHeight是可视区的高度
+                let windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+                //变量scrollHeight是滚动条的总高度
+                let scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
+                //滚动条到底部的条件
+                if(scrollTop+windowHeight==scrollHeight){
+                    return true;
+                }
+                return false;
+            },
+            //追加数据
+            AdditionalData(){
+                //无法动态刷新循环
+                // while(this.TouchBottom()){
+                //     debugger
+                //     this._data.myrownum++;
+                // }
+            }
         }
     }
 </script>
