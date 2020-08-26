81d65c9eda1460592a4c07fa23b35de69c036b78
ni <243817437@qq.com>
2020-08-26 18:16:36 +0800

    日常

--- src/test/java/com/example/shirotest/qwer.java
+++ src/test/java/com/example/shirotest/qwer.java
@@ -1,10 +1,12 @@
 package com.example.shirotest;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.util.UUID;
 import java.util.concurrent.*;
 
@@ -30,8 +32,23 @@ public class qwer {
 //            int aa=10;
 //        }
 //        pool.shutdown();
+        IOTest io=new IOTest();
+        io.moveFile(log);
         TestInter tes=()->{return "222";};
         log.debug(tes.get());
+    }
+}
+
+class IOTest{
+    public void moveFile(Logger log){
+        File a1=new File("c:\\work\\Weex培训文档.zip");
+        File a2=new File("C:\\work\\filetest"+File.separator+a1.getName());
+        try{
+            FileUtils.copyFile(a2,a1);
+            //a1.renameTo(a2);
+        }catch(Exception IO){
+            log.debug(IO.getMessage());
+        }
     }
 }
 
