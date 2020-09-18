59bdc922d8910dc943fea6447aaa03a6977aa3a6
xrr <xrr@qq.com>
2020-09-18 17:25:05 +0800

    日常

--- src/main/java/com/example/shirotest/TestClass.java
+++ src/main/java/com/example/shirotest/TestClass.java
@@ -5,17 +5,25 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 
 public class TestClass {
-//    public static void main(String[] a)
-//    {
-//        Subject currUser= SecurityUtils.getSubject();
-//        if(!currUser.isAuthenticated())
-//        {
-//            UsernamePasswordToken up=new UsernamePasswordToken("123","456");
-//            currUser.login(up);
-//        }
-//        currUser.getPrincipal();
-//    }
+    public static void main(String[] a)
+    {
+        ArrayList aa=new ArrayList();
+        aa.add(2);
+        aa.add(3);
+        aa.add(5);
+        aa.add(1);
+        aa.add(-5);
+        aa.sort((x,y)->{
+            return Integer.parseInt(y.toString())-Integer.parseInt(x.toString());
+        });
+        int xx=10;
+    }
 
 }
