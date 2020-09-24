7fc1afb55a9d3c7c9255a7b2caa2634cc6c65ed8
Administrator <243817437@qq.com>
2020-09-24 22:23:35 +0800

    日常

--- src/main/java/com/example/shirotest/TestClass.java
+++ src/main/java/com/example/shirotest/TestClass.java
@@ -1,14 +1,13 @@
 package com.example.shirotest;
 
 
+import javafx.beans.binding.IntegerBinding;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -24,7 +23,55 @@ public class TestClass {
         aa.sort((x,y)->{
             return Integer.parseInt(y.toString())-Integer.parseInt(x.toString());
         });
-        int xx=10;
+        int[] xx={50,47,68,33,35,84,25,49,91,75};
+        minTime(xx,1);
+    }
+
+    public static int minTime(int[] time, int m) {
+        int xx=dep(time,m);
+        return xx;
+    }
+
+    public static int dep(int[] time, int m){
+        if(time.length<=m){
+            return 0;
+        }
+        if(time.length==m+1){
+            Arrays.sort(time);
+            return time[0];
+        }
+        List list=new ArrayList<int[]>();
+        for(int i=0;i<time.length-1;i++){
+            String tmp=String.valueOf(time[i]+time[i+1])+"-"+String.valueOf(i);
+            list.add(tmp);
+        }
+        Collections.sort(list,(a,b)->{
+            return Integer.parseInt(a.toString().split("-")[0])-Integer.parseInt(b.toString().split("-")[0]);
+        });
+        int min=Integer.parseInt(list.get(0).toString().split("-")[0]);
+        int minIndex=Integer.parseInt(list.get(0).toString().split("-")[1]);
+        int[] tmptime=Arrays.copyOfRange(time,0,minIndex);
+        List tl=new ArrayList();
+        if(tmptime.length>0){
+            for(int i:tmptime){
+                tl.add(i);
+            }
+        }
+        tl.add(min);
+        if(minIndex+2<=time.length){
+            int[] tmp=Arrays.copyOfRange(time,minIndex+2,time.length);
+            if(tmp.length>0){
+                for(int i:tmp){
+                    tl.add(i);
+                }
+            }
+        }
+        int[] ta=(int[]) Array.newInstance(int.class, tl.toArray().length);
+        int c=0;
+        for(Object x:tl.toArray()){
+            ta[c++]=(int)x;
+        }
+        return dep(ta,m);
     }
 
 }
