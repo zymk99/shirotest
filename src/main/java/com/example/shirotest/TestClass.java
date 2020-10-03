37a57f90b1c42ea0f6b139c9eb99fec901918265
Administrator <243817437@qq.com>
2020-10-03 22:49:45 +0800

    日常

--- src/main/java/com/example/shirotest/TestClass.java
+++ src/main/java/com/example/shirotest/TestClass.java
@@ -23,54 +23,51 @@ public class TestClass {
         aa.sort((x,y)->{
             return Integer.parseInt(y.toString())-Integer.parseInt(x.toString());
         });
-        List<Integer> aaa=splitIntoFibonacci("214748364721474836422147483641");
+        int aaa=minimumOperations("yry");
         int xx=10;
 
     }
-    public static List<Integer> splitIntoFibonacci(String S) {
-        List<Integer> list=new ArrayList<>();
-        List<Integer> objlist=new ArrayList<>();
-        int l= S.length();
-        if(l>=3){
-            for(int i=1;i<l-1&&i<=9;i++){
-                String s1=S.substring(0,i);
-                if(s1.length()>1 && Integer.parseInt(s1)!=0 && s1.indexOf('0')==0){
-                    continue;
-                }
-                for(int j=i+1;j<l&&j<=i+9;j++){
-                    String s2=S.substring(i,j);
-                    if(s2.length()>1 && Integer.parseInt(s2)!=0 &&s2.indexOf('0')==0){
-                        continue;
-                    }
-                    list=new ArrayList<>();
-                    list.add(Integer.parseInt(s1));
-                    list.add(Integer.parseInt(s2));
-                    objlist=db(Integer.parseInt(s1),Integer.parseInt(s2),S.substring(j,l),list,objlist);
-                    if(objlist.size()>0){
-                        return objlist;
-                    }
-                }
-            }
+    public static int minimumOperations(String leaves) {
+        int x=0;
+        if(!leaves.startsWith("r")){
+            x++;
         }
-        return objlist;
+        if(!leaves.endsWith("r")){
+            x++;
+        }
+        x+=db(leaves.substring(1,leaves.length()-1),x);
+        return x;
     }
 
-    public static List<Integer> db(int a,int b,String s,List<Integer> list,List<Integer> objlist){
-        if(s.length()==0){
-            objlist= (ArrayList)((ArrayList)list).clone();
-            return objlist;
-        }
-        for(int i=1;i<s.length()+1 && i<=9;i++){
-            if(i>1 && s.substring(0,i).indexOf('0')==0){
-                continue;
-            }
-            if(a+b==Integer.parseInt(s.substring(0,i))){
-                list.add(Integer.parseInt(s.substring(0,i)));
-                objlist=db(b,Integer.parseInt(s.substring(0,i)),s.substring(i,s.length()),list,objlist);
-                list.remove(list.size()-1);
+    public static int db(String s,int num){
+        if(s.length()==0)return 0;
+        if(s.length()==1 || s.length()==2){
+            if(s.indexOf("y")>=0){
+                return 0;
+            }else
+            {
+                return 1;
             }
         }
-        return objlist;
+        int all=0;
+        for(char c : s.toCharArray()){
+            if(c=='r')
+                all++;
+        }
+        int x1=0,x2=0,x3=0;
+        if(!s.startsWith("r")){
+            x1++;
+        }
+        if(!s.endsWith("r")){
+            x2++;
+        }
+        x3=x1+x2;
+        x1+=db(s.substring(1,s.length()),num+x1);
+        x2+=db(s.substring(0,s.length()-1),num+x2);
+        x3+=db(s.substring(1,s.length()-1),num+x1+x2);
+        int val=Math.min(all,Math.min(x1,Math.min(x2,x3)));
+        return val;
     }
+
 
 }
