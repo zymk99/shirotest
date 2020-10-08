1e7d5c5f0b2d3341b86aef7e429d171bfe8ec2b2
Administrator <243817437@qq.com>
2020-10-08 15:40:52 +0800

    日常

--- src/main/java/com/example/shirotest/TestClass.java
+++ src/main/java/com/example/shirotest/TestClass.java
@@ -24,42 +24,35 @@ public class TestClass {
         aa.sort((x,y)->{
             return Integer.parseInt(y.toString())-Integer.parseInt(x.toString());
         });
-        List<String> list=new ArrayList<>();
-        list.add("cats");
-        list.add("dog");
-        list.add("sand");
-        list.add("and");
-        boolean aaa=wordBreak("catsandog",list);
+        int aaa=numTilePossibilities("A");
         int xx=10;
     }
-    public static boolean wordBreak(String s, List<String> wordDict) {
-        if(wordDict.size()==0){
-            return false;
-        }
-        boolean val=db(s,wordDict);
-        return val;
-    }
-    public static boolean db(String s,List<String> list){
-        HashMap map=new HashMap<String,Integer>();
-        list.stream().forEach(item->{
-            map.put(item,item.length());
-        });
-        Boolean[] bs=new Boolean[s.length()+1];
-        bs[0]=true;
-        int i=1;
-        for(i=1;i<bs.length;i++){
-            boolean bol=false;
-            Iterator it=map.keySet().iterator();
-            while(it.hasNext()){
-                String v=it.next().toString();
-                if(i-v.length()>=0 && s.substring(i-v.length(),i).equals(v) && bs[i-v.length()]==true){
-                    bol=true;
-                    break;
-                }
+    public static int numTilePossibilities(String tiles) {
+        char[] cs=tiles.toCharArray();
+        HashMap map=new HashMap();
+        for(char c:cs){
+            if(map.get(c)!=null){
+                map.put(c, (int)map.get(c)+1);
+            }else{
+                map.put(c, 1);
             }
-            bs[i]=bol;
         }
-        return bs[bs.length-1];
+        int v=db(map)-1;
+        return v;
+    }
+    public static int db(HashMap map){
+        int num=1;
+        Iterator it=map.keySet().iterator();
+        while(it.hasNext()){
+            char c=(char)it.next();
+            int x=(int)map.get(c);
+            if((int)map.get(c) >0){
+                map.put(c,x-1);
+                num=num+db(map);
+                map.put(c,x);
+            }
+        }
+        return num;
     }
 
 
