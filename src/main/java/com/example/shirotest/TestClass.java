a920ac9465d677bb4a470f4a64bee920f425882b
Administrator <243817437@qq.com>
2020-10-09 21:55:36 +0800

    日常

--- src/main/java/com/example/shirotest/TestClass.java
+++ src/main/java/com/example/shirotest/TestClass.java
@@ -10,6 +10,7 @@ import org.apache.shiro.subject.Subject;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class TestClass {
@@ -24,42 +25,41 @@ public class TestClass {
         aa.sort((x,y)->{
             return Integer.parseInt(y.toString())-Integer.parseInt(x.toString());
         });
-        List<String> list=new ArrayList<String>();
-        list.add("ac");
-        list.add("ab");
-        list.add("aaa");
-        String aaa=replaceWords(list,"it is abnormal that this solution is accepted");
+        char[] c={'A','A','A','B','B','B','c','c','c','D','D','E'};
+        int aaa=leastInterval(c,2);
         int xx=10;
     }
-    public static String replaceWords(List<String> dictionary, String sentence) {
-        if(dictionary.size()==0){
-            return sentence;
-        }else if(sentence.length()==0){
-            return sentence;
+    public static int leastInterval(char[] tasks, int n) {
+        HashMap map=new HashMap();
+        for(char c :tasks){
+            map.put(c, map.get(c)==null?1 : Integer.parseInt(map.get(c).toString())+1 );
         }
-        Collections.sort(dictionary,(item1,item2)->{
-            return item1.length()-item2.length();
+        ArrayList list=new ArrayList();
+        Iterator it=map.keySet().iterator();
+        while(it.hasNext()){
+            char c=(char)it.next();
+            list.add(String.valueOf(c)+"-"+map.get(c).toString());
+        }
+        Collections.sort(list,(a1,a2)->{
+            return Integer.valueOf(a2.toString().split("-")[1])-Integer.valueOf(a1.toString().split("-")[1]);
         });
-        String[] ss=sentence.split(" ");
-        for(int i=0;i<ss.length;i++){
-            if(ss[i].length()>0){
-                Iterator it=dictionary.iterator();
-                while (it.hasNext()){
-                    String tmp=(String)it.next();
-                    if(ss[i].length()<tmp.length())continue;
-                    if(ss[i].startsWith(tmp)){
-                        ss[i]=tmp;
-                        break;
-                    }
-                }
+        char[] cs=new char[1000002];
+        Iterator iter=list.iterator();
+        while(iter.hasNext()){
+            String tmps=iter.next().toString();
+            int x=Integer.valueOf(tmps.split("-")[1]);
+            int index=0;
+            while(cs[index++]!='\0');
+            index--;
+            for(;x>0;x--,index=index+n+1){
+                cs[index]=tmps.split("-")[0].toCharArray()[0];
             }
         }
-        String v="";
-        for(String t:ss){
-            v += t+" ";
+        int v=1000000;
+        for(;v>=0;v--){
+            if(cs[v]!='\0')break;
         }
-        v=v.substring(0,v.length()-1);
-        return v;
+        return v+1;
     }
     public static int db(HashMap map){
         int num=1;
